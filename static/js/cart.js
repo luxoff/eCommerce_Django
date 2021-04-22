9b14d92dd37d23b45e80d44c4c3584887a579099
luxoff <dima4365@gmail.com>
2021-04-22 11:27:49 +0300

    improved cart func, added a checkout functionality

---
+++ static/js/cart.js
@@ -0,0 +1,63 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (i = 0; i < updateBtns.length; i++) {
+	updateBtns[i].addEventListener('click', function(){
+		var prodId = this.dataset.product
+		var action = this.dataset.action
+		console.log('productId:', prodId, 'Action:', action)
+		console.log('USER:', user)
+
+		if (user == 'AnonymousUser'){
+			addCookieItem(prodId, action)
+		}else{
+			updateUserOrder(prodId, action)
+		}
+	})
+}
+
+function updateUserOrder(prodId, action){
+	console.log('User is authenticated, sending data...')
+
+		var url = '/updateItem/'
+
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+				'X-CSRFToken':csrftoken,
+			},
+			body:JSON.stringify({'prodId':prodId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    location.reload()
+		});
+}
+
+function addCookieItem(prodId, action){
+	console.log('User is not authenticated')
+
+	if (action == 'add'){
+		if (cart[prodId] == undefined){
+		cart[prodId] = {'quantity':1}
+
+		}else{
+			cart[prodId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove'){
+		cart[prodId]['quantity'] -= 1
+
+		if (cart[prodId]['quantity'] <= 0){
+			console.log('Item should be deleted')
+			delete cart[prodId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+
+	location.reload()
+}
--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
+
+    path('updateItem/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="process_order"),
 ]
